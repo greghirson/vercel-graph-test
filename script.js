5eb928054931dc3eec7ed4ad37433b8a0c7d7ece
Greg Hirson <greg.hirson@gmail.com>
2025-06-09 18:00:01 +0000

    new data at 09/06/2025 18:00:01

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
       $schema: "https://vega.github.io/schema/vega-lite/v5.json",
       description: "CO2 sensor data chart",
       data: {
-        values: [{"a": "2025-06-07 20:25:00", "b": 2689.5}],
+        values: [{"a": "2025-06-09 10:05:00", "b": 2405.5}, {"a": "2025-06-09 10:10:00", "b": 4890.5}],
       },
       mark: "line",
       width: "container",
